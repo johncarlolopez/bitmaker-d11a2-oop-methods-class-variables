04463eefc94c21528c738e8c17edcd06c333455b
John Carlo Lopez <condottiere.jc@gmail.com>
2017-12-18 14:28:35 -0500

    initialize exercise 2

--- bankaccount.rb
+++ bankaccount.rb
@@ -44,20 +44,3 @@ class BankAccount
 
  end
 end
-
-my_account = BankAccount.create
-your_account = BankAccount.create
-puts my_account.balance # 0
-puts BankAccount.total_funds # 0
-my_account.deposit(200)
-your_account.deposit(1000)
-puts my_account.balance # 200
-puts your_account.balance # 1000
-puts BankAccount.total_funds # 1200
-BankAccount.interest_time
-puts my_account.balance # 202.0
-puts your_account.balance # 1010.0
-puts BankAccount.total_funds # 1212.0
-my_account.withdraw(50)
-puts my_account.balance # 152.0
-puts BankAccount.total_funds # 1162.0
